a37f10f370a21c590308a656e17a293a5c15281d
Sebastian P.R. Gingter <sebastian@gingter.org>
2017-10-15 11:17:46 +0200

    Add basic webserver

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+import http = require('http');
 
 console.log('Hallo EKON from Typescript!');
+
+const server = new http.Server((req, res) => {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('OK from EKON');
+});
+
+server.listen(8080);
