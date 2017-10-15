5f63db1bbfee8dda1a752b00afea6b4fb287a975
Sebastian P.R. Gingter <sebastian@gingter.org>
2017-10-15 13:18:09 +0200

    Add database infrastructure

---
+++ src/database/index.ts
@@ -0,0 +1,48 @@
+import { Connection, createConnection } from 'typeorm';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/mysqlconnectionoptions';
+
+export interface DatabaseConfiguration {
+	type: 'mysql' | 'mariadb' ;
+	host: string;
+	port: number;
+	username: string;
+	password: string;
+	database: string;
+	ssl?: boolean;
+}
+
+export class DatabaseProvider {
+
+	private static connection: Connection;
+	private static configuration: DatabaseConfiguration;
+
+	public static configure(databaseConfiguration: DatabaseConfiguration): void {
+		DatabaseProvider.configuration = databaseConfiguration;
+	}
+
+	public static async getConnection(): Promise<Connection> {
+
+		if (DatabaseProvider.connection) {
+			return DatabaseProvider.connection;
+		}
+
+		if (!DatabaseProvider.configuration) {
+			throw new Error('DatabaseProvider is not configured yet.');
+		}
+
+		const { type, host, port, username, password, database, ssl } = DatabaseProvider.configuration;
+
+		DatabaseProvider.connection = await createConnection({
+			type, host, port, username, password, database,
+			extra: {
+				ssl
+			},
+			entities: [
+				// TODO: Add entities
+			],
+			synchronize: true,
+		} as MysqlConnectionOptions);
+
+		return DatabaseProvider.connection;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import 'reflect-metadata';
+import { DatabaseProvider } from './database/index';
+
+DatabaseProvider.configure({
+	type: process.env.DATABASE_TYPE as any || 'mariadb',
+	database: process.env.DATABASE_NAME || 'ekon',
+	username: process.env.DATABASE_USERNAME || 'ekon',
+	password: process.env.DATABASE_PASSWORD || 'ekon',
+	host: process.env.DATABASE_HOST || 'localhost',
+	port: +process.env.DATABASE_PORT || 3306,
+	ssl: !!process.env.USE_SSL
+});
 
 console.log('Hallo EKON from Typescript!');
